2c513e4fe3ded0fc1146bbb490922089ecdbae64
eduardxs <134706741+eduardxs@users.noreply.github.com>
2024-04-24 10:49:26 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -10,5 +10,14 @@ botoes[i].onclick = function(){
 for(let j=0;j<botoes.length;j++){
     botoes[j].classList.remove("ativo");
 }
+{
+    
+}
 
+
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
+let tempoAtual = new Date();
+
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
 
